37541ae146ef2ada9fcf08486f4ec7543b6ad6f5
ANDRES CAMILO OÑATE QUIMBAYO <63562181+AndresOnate@users.noreply.github.com>
2022-05-24 18:57:19 -0500

    Add files via upload

---
+++ Laboratorios/Lab 05/poblar.sql
@@ -0,0 +1,187 @@
+--Usuarios
+insert into usuarios (id, email, name, createdAt) values (1, 'phelder0@furl.net', 'Patrizia Helder', '07/12/2010');
+insert into usuarios (id, email, name, createdAt) values (2, 'lmcewen1@drupal.org', 'Lyle McEwen', '25/08/2008');
+insert into usuarios (id, email, name, createdAt) values (3, 'mwoodwind2@nytimes.com', 'Murial Woodwind', '25/11/2007');
+insert into usuarios (id, email, name, createdAt) values (4, 'mfrancino3@tinypic.com', 'Mauricio Francino', '11/01/2007');
+insert into usuarios (id, email, name, createdAt) values (5, 'asimmens4@fotki.com', 'Allissa Simmens', '26/06/2017');
+insert into usuarios (id, email, name, createdAt) values (6, 'lboggs5@amazon.com', 'Liana Boggs', '21/04/2007');
+insert into usuarios (id, email, name, createdAt) values (7, 'wpeterson6@hp.com', 'Wileen Peterson', '09/10/2014');
+insert into usuarios (id, email, name, createdAt) values (8, 'jdempsey7@xrea.com', 'Jemimah Dempsey', '10/11/2021');
+insert into usuarios (id, email, name, createdAt) values (9, 'mlowfill8@behance.net', 'Marsh Lowfill', '15/11/2020');
+insert into usuarios (id, email, name, createdAt) values (10, 'apeasey9@who.int', 'Alaine Peasey', '27/11/2019');
+insert into usuarios (id, email, name, createdAt) values (11, 'canana@wikipedia.org', 'Chelsy Anan', '14/08/2010');
+insert into usuarios (id, email, name, createdAt) values (12, 'klazerb@soup.io', 'Kelley Lazer', '09/09/2008');
+insert into usuarios (id, email, name, createdAt) values (13, 'mgarryc@redcross.org', 'Mendie Garry', '17/01/2011');
+insert into usuarios (id, email, name, createdAt) values (14, 'eclerked@ucoz.com', 'Esdras Clerke', '27/12/2006');
+insert into usuarios (id, email, name, createdAt) values (15, 'maucotte@pinterest.com', 'Merrili Aucott', '14/08/2008');
+insert into usuarios (id, email, name, createdAt) values (16, 'bgorhamf@cmu.edu', 'Baxie Gorham', '26/07/2020');
+insert into usuarios (id, email, name, createdAt) values (17, 'bhakingg@gnu.org', 'Brandie Haking', '04/06/2018');
+insert into usuarios (id, email, name, createdAt) values (18, 'gbignoldh@moonfruit.com', 'Gaven Bignold', '06/05/2017');
+insert into usuarios (id, email, name, createdAt) values (19, 'bkulleri@github.io', 'Bertine Kuller', '10/04/2010');
+insert into usuarios (id, email, name, createdAt) values (20, 'mthackhamj@51.la', 'Mead Thackham', '04/01/2018');
+insert into usuarios (id, email, name, createdAt) values (21, 'Stiven@.maila', 'Steven Grant', '04/01/2018');
+--- cuentas
+insert into cuentas (id, name, createdAt, suscribers, usuarios_id) values (133, 'alocarno0', '30/05/2019', 27857, 1);
+insert into cuentas (id, name, createdAt, suscribers, usuarios_id) values (258, 'ftzuker1', '04/03/2018', 97632, 2);
+insert into cuentas (id, name, createdAt, suscribers, usuarios_id) values (383, 'ebraunston2', '08/01/2021', 64857, 3);
+insert into cuentas (id, name, createdAt, suscribers, usuarios_id) values (508, 'sremmer3', '30/12/2011', 96601, 4);
+insert into cuentas (id, name, createdAt, suscribers, usuarios_id) values (633, 'lgerge4', '14/06/2016', 5933, 5);
+insert into cuentas (id, name, createdAt, suscribers, usuarios_id) values (758, 'ccappell5', '24/11/2007', 23138, 6);
+insert into cuentas (id, name, createdAt, suscribers, usuarios_id) values (883, 'pgrimsley6', '11/04/2014', 97277, 7);
+insert into cuentas (id, name, createdAt, suscribers, usuarios_id) values (1008, 'rswindon7', '30/05/2019', 69594, 8);
+insert into cuentas (id, name, createdAt, suscribers, usuarios_id) values (1133, 'ccleynman8', '05/01/2015', 5853, 9);
+insert into cuentas (id, name, createdAt, suscribers, usuarios_id) values (1258, 'mhebdon9', '22/08/2007', 47347, 10);
+insert into cuentas (id, name, createdAt, suscribers, usuarios_id) values (1383, 'jmckimma', '27/02/2007', 31582, 11);
+insert into cuentas (id, name, createdAt, suscribers, usuarios_id) values (1508, 'fparmeterb', '19/01/2015', 70957, 12);
+insert into cuentas (id, name, createdAt, suscribers, usuarios_id) values (1633, 'cgalloc', '15/03/2012', 2418, 13);
+insert into cuentas (id, name, createdAt, suscribers, usuarios_id) values (1758, 'cbrideoked', '01/04/2019', 83223, 14);
+insert into cuentas (id, name, createdAt, suscribers, usuarios_id) values (1883, 'coregane', '12/12/2014', 29173, 15);
+insert into cuentas (id, name, createdAt, suscribers, usuarios_id) values (2008, 'kandresf', '19/11/2005', 36528, 16);
+insert into cuentas (id, name, createdAt, suscribers, usuarios_id) values (2133, 'dweing', '02/08/2019', 85604, 17);
+insert into cuentas (id, name, createdAt, suscribers, usuarios_id) values (2258, 'lcatfordh', '17/11/2007', 68361, 18);
+insert into cuentas (id, name, createdAt, suscribers, usuarios_id) values (2383, 'fbelamyi', '03/07/2005', 27063, 19);
+insert into cuentas (id, name, createdAt, suscribers, usuarios_id) values (2508, 'lkezarj', '01/10/2005', 33036, 20);
+insert into cuentas (id, name, createdAt, suscribers, usuarios_id) values (1, 'Coffe TV', '03/07/2005', 27063, 21);
+insert into cuentas (id, name, createdAt, suscribers, usuarios_id) values (2, 'Te lo resumo', '01/10/2005', 33036, 22);
+insert into cuentas (id, name, createdAt, suscribers, usuarios_id) values (3, 'Steven G', '03/07/2005', 27063, 98);
+
+-- exclusividades
+insert into exclusividades (code, e_order, name, price, duration, cuentas_id) values ('EX-827439', 955, 'Witch Hazel', 856753545, 85, 133);
+insert into exclusividades (code, e_order, name, price, duration, cuentas_id) values ('EX-522956', 675, 'Chaetomium globosum', 265106392, 57, 258);
+insert into exclusividades (code, e_order, name, price, duration, cuentas_id) values ('EX-503852', 292, 'Norethindrone Acetate', 545820119, 37, 383);
+insert into exclusividades (code, e_order, name, price, duration, cuentas_id) values ('EX-263208', 333, 'Abstral', 474518564, 70, 508);
+insert into exclusividades (code, e_order, name, price, duration, cuentas_id) values ('EX-549368', 172, 'Venlafaxine Hydrochloride', 894627838, 42, 633);
+insert into exclusividades (code, e_order, name, price, duration, cuentas_id) values ('EX-946751', 914, 'WINRHO', 517275827, 2, 758);
+insert into exclusividades (code, e_order, name, price, duration, cuentas_id) values ('EX-221459', 571, 'pain relief', 240802562, 61, 883);
+insert into exclusividades (code, e_order, name, price, duration, cuentas_id) values ('EX-825903', 45, 'Maximum Strength', 802636775, 76, 1008);
+insert into exclusividades (code, e_order, name, price, duration, cuentas_id) values ('EX-745070', 474, 'Lavender Hand Sanitizer', 234655698, 19, 1133);
+insert into exclusividades (code, e_order, name, price, duration, cuentas_id) values ('EX-385222', 847, 'BYDUREON', 519844217, 51, 1258);
+insert into exclusividades (code, e_order, name, price, duration, cuentas_id) values ('EX-085741', 615, 'Metronidazole', 317951798, 6, 1383);
+insert into exclusividades (code, e_order, name, price, duration, cuentas_id) values ('EX-371094', 22, 'Glucagon', 116339545, 75, 1508);
+insert into exclusividades (code, e_order, name, price, duration, cuentas_id) values ('EX-125003', 883, 'Laxative', 17654374, 25, 1633);
+insert into exclusividades (code, e_order, name, price, duration, cuentas_id) values ('EX-590109', 966, 'Isoniazid', 321433042, 67, 1758);
+insert into exclusividades (code, e_order, name, price, duration, cuentas_id) values ('EX-383236', 617, 'Carvedilol', 452655210, 10, 1883);
+insert into exclusividades (code, e_order, name, price, duration, cuentas_id) values ('EX-794114', 947, 'Ciclopirox', 748751924, 49, 2008);
+insert into exclusividades (code, e_order, name, price, duration, cuentas_id) values ('EX-820033', 345, 'Rhus Tox. e fol. 4', 737791153, 14, 2133);
+insert into exclusividades (code, e_order, name, price, duration, cuentas_id) values ('EX-207507', 456, 'Alprazolam', 300030473, 37, 2258);
+insert into exclusividades (code, e_order, name, price, duration, cuentas_id) values ('EX-879607', 291, 'Nitroglycerin In Dextrose', 531252159, 23, 2383);
+insert into exclusividades (code, e_order, name, price, duration, cuentas_id) values ('EX-268539', 148, 'Caduet', 381766026, 3, 2508);
+
+
+
+
+-- suscripciones
+insert into suscripciones (id, createdAt, detail, cuentas_id, suscritoA) values ('', '', 'Prueba Lab 05', 2508, 133);
+insert into suscripciones (id, createdAt, detail, cuentas_id, suscritoA) values ('', '', 'Steven Subs 1', 3, 1);
+insert into suscripciones (id, createdAt, detail, cuentas_id, suscritoA) values ('', '', 'Steven Subs 2', 3, 2);
+insert into suscripciones (id, createdAt, detail, cuentas_id, suscritoA) values (223, '05/07/2019', 'nec molestie sed', 133, 2508);
+insert into suscripciones (id, createdAt, detail, cuentas_id, suscritoA) values (446, '03/07/2011', 'vitae mattis nibh ligula', 258, 2383);
+insert into suscripciones (id, createdAt, detail, cuentas_id, suscritoA) values (669, '20/06/2013', null, 383, 2258);
+insert into suscripciones (id, createdAt, detail, cuentas_id, suscritoA) values (892, '17/12/2019', null, 508, 2133);
+insert into suscripciones (id, createdAt, detail, cuentas_id, suscritoA) values (1115, '03/10/2014', null, 633, 2008);
+insert into suscripciones (id, createdAt, detail, cuentas_id, suscritoA) values (1338, '14/12/2008', 'sit amet', 758, 1883);
+insert into suscripciones (id, createdAt, detail, cuentas_id, suscritoA) values (1561, '12/04/2021', 'eget vulputate ut ultrices vel', 883, 1758);
+insert into suscripciones (id, createdAt, detail, cuentas_id, suscritoA) values (1784, '20/05/2021', 'leo maecenas pulvinar', 1008, 1633);
+insert into suscripciones (id, createdAt, detail, cuentas_id, suscritoA) values (2007, '04/11/2012', 'elit ac', 1133, 1508);
+insert into suscripciones (id, createdAt, detail, cuentas_id, suscritoA) values (2230, '08/10/2019', null, 1258, 1383);
+insert into suscripciones (id, createdAt, detail, cuentas_id, suscritoA) values (2453, '25/04/2018', null, 1383, 1258);
+insert into suscripciones (id, createdAt, detail, cuentas_id, suscritoA) values (2676, '17/04/2010', 'ut erat curabitur', 1508, 1133);
+insert into suscripciones (id, createdAt, detail, cuentas_id, suscritoA) values (2899, '17/01/2018', 'ut erat id', 1633, 1008);
+insert into suscripciones (id, createdAt, detail, cuentas_id, suscritoA) values (3122, '28/03/2014', 'nisi volutpat eleifend donec ut dolor', 1758, 883);
+insert into suscripciones (id, createdAt, detail, cuentas_id, suscritoA) values (3345, '01/01/2009', 'sapien non mi', 1883, 758);
+insert into suscripciones (id, createdAt, detail, cuentas_id, suscritoA) values (3568, '12/02/2019', 'lacus at velit vivamus vel', 2008, 633);
+insert into suscripciones (id, createdAt, detail, cuentas_id, suscritoA) values (3791, '24/07/2021', null, 2133, 508);
+insert into suscripciones (id, createdAt, detail, cuentas_id, suscritoA) values (4014, '25/10/2007', 'luctus et', 2258, 383);
+insert into suscripciones (id, createdAt, detail, cuentas_id, suscritoA) values (4237, '20/07/2013', 'pellentesque volutpat dui maecenas tristique', 2383, 258);
+insert into suscripciones (id, createdAt, detail, cuentas_id, suscritoA) values (4460, '12/06/2015', 'vel accumsan tellus nisi eu', 2508, 133);
+insert into suscripciones (id, createdAt, detail, cuentas_id, suscritoA) values ('', '', 'Steven Subs 3', 3, 133);
+
+-- etapas
+insert into etapas (startAt, endAt, price, status, exclusividades_id, exclusividades_order, suscripciones_id) values ('28/07/2010', null, 614483351, 'Terminada', 133, 955, 1);
+insert into etapas (startAt, endAt, price, status, exclusividades_id, exclusividades_order, suscripciones_id) values ('18/11/2008', '09/06/2021', 267433811, 'Terminada', 258,675, 2);
+insert into etapas (startAt, endAt, price, status, exclusividades_id, exclusividades_order, suscripciones_id) values ('02/12/2013', '02/06/2016', 94064248, 'Terminada', 383,292, 3);
+insert into etapas (startAt, endAt, price, status, exclusividades_id, exclusividades_order, suscripciones_id) values ('08/12/2007', '24/06/2021', 295718347, 'Cancelada', 508,333, 4);
+insert into etapas (startAt, endAt, price, status, exclusividades_id, exclusividades_order, suscripciones_id) values ('20/08/2008', null, 815069414, 'Terminada', 633, 172,5);
+insert into etapas (startAt, endAt, price, status, exclusividades_id, exclusividades_order, suscripciones_id) values ('15/02/2012', '26/02/2021', 511203890, 'Terminada', 758,914, 6);
+insert into etapas (startAt, endAt, price, status, exclusividades_id, exclusividades_order, suscripciones_id) values ('04/01/2006', '28/09/2017', 824847852, 'Activa', 883,571, 7);
+insert into etapas (startAt, endAt, price, status, exclusividades_id, exclusividades_order, suscripciones_id) values ('04/01/2010', null, 952591658, 'Activa', 1008, 45,8);
+insert into etapas (startAt, endAt, price, status, exclusividades_id, exclusividades_order, suscripciones_id) values ('26/03/2008', '06/02/2020', 696215544, 'Activa', 1133, 474, 9);
+insert into etapas (startAt, endAt, price, status, exclusividades_id, exclusividades_order, suscripciones_id) values ('25/02/2014', '30/05/2020', 741785490, 'Cancelada', 1258,847, 10);
+insert into etapas (startAt, endAt, price, status, exclusividades_id, exclusividades_order, suscripciones_id) values ('28/04/2022', '31/12/2022', 741785491, 'Activa', 1258,847, 2);
+insert into etapas (startAt, endAt, price, status, exclusividades_id, exclusividades_order, suscripciones_id) values ('28/04/2022', '31/12/2022', 741785491, 'Activa', 1258,847,3);
+-- contenido
+insert into contenidos (id, title, publishingDate, description, usuarios_id, exclusividades_id) values (3477, 'sed', '15/05/2008', 'bibendum felis', 1, 133);
+insert into contenidos (id, title, publishingDate, description, usuarios_id, exclusividades_id) values (7037, 'est', '02/01/2020', null, 2, 258);
+insert into contenidos (id, title, publishingDate, description, usuarios_id, exclusividades_id) values (10597, 'id', '10/02/2007', 'in hac', 3, 383);
+insert into contenidos (id, title, publishingDate, description, usuarios_id, exclusividades_id) values (14157, 'scelerisque', '16/11/2021', 'urna ut tellus', 4, 508);
+insert into contenidos (id, title, publishingDate, description, usuarios_id, exclusividades_id) values (17717, 'platea', '22/06/2013', null, 5, 633);
+insert into contenidos (id, title, publishingDate, description, usuarios_id, exclusividades_id) values (21277, 'in', '22/05/2019', null, 6, 758);
+insert into contenidos (id, title, publishingDate, description, usuarios_id, exclusividades_id) values (24837, 'sollicitudin vitae', '17/10/2006', 'est et tempus', 7, 883);
+insert into contenidos (id, title, publishingDate, description, usuarios_id, exclusividades_id) values (28397, 'interdum mauris', '16/04/2015', 'augue vel accumsan', 8, 1008);
+insert into contenidos (id, title, publishingDate, description, usuarios_id, exclusividades_id) values (31957, 'vestibulum quam', '31/03/2016', 'posuere nonummy integer', 9, 1133);
+insert into contenidos (id, title, publishingDate, description, usuarios_id, exclusividades_id) values (35517, 'elit', '17/08/2011', null, 10, 1258);
+insert into contenidos (id, title, publishingDate, description, usuarios_id, exclusividades_id) values (39077, 'nunc', '08/10/2009', 'lectus pellentesque at', 11, 1383);
+insert into contenidos (id, title, publishingDate, description, usuarios_id, exclusividades_id) values (42637, 'odio', '09/01/2013', 'libero quis', 12, 1508);
+insert into contenidos (id, title, publishingDate, description, usuarios_id, exclusividades_id) values (46197, 'morbi vestibulum', '16/09/2016', 'eget eleifend luctus', 13, 1633);
+insert into contenidos (id, title, publishingDate, description, usuarios_id, exclusividades_id) values (49757, 'odio consequat', '27/07/2016', 'eget tincidunt eget', 14, 1758);
+insert into contenidos (id, title, publishingDate, description, usuarios_id, exclusividades_id) values (53317, 'mus etiam', '07/04/2009', 'etiam faucibus', 15, 1883);
+
+-- videos
+insert into videos (contenidos_id, duration) values (3477, 168);
+insert into videos (contenidos_id, duration) values (7037, 1296);
+insert into videos (contenidos_id, duration) values (10597, 354);
+insert into videos (contenidos_id, duration) values (14157, 487);
+insert into videos (contenidos_id, duration) values (17717, 166);
+
+--streamings
+insert into streamings (contenidos_id, plannedDate, actualDate, duration) values (21277, '28/12/2006', '03/08/2020', 386);
+insert into streamings (contenidos_id, plannedDate, actualDate, duration) values (24837, '29/12/2006', null, 705);
+insert into streamings (contenidos_id, plannedDate, actualDate, duration) values (28397, '16/03/2007', '13/01/2019', 1123);
+insert into streamings (contenidos_id, plannedDate, actualDate, duration) values (31957, '10/05/2012', '25/04/2020', 952);
+insert into streamings (contenidos_id, plannedDate, actualDate, duration) values (35517, '28/08/2007', null, 599);
+
+--posts
+insert into posts (contenidos_id, text) values (39077, 'ullamcorper augue a suscipit nulla elit');
+insert into posts (contenidos_id, text) values (42637, 'sit amet diam in magna');
+insert into posts (contenidos_id, text) values (46197, 'lobortis ligula sit amet');
+insert into posts (contenidos_id, text) values (49757, 'phasellus id sapien in sapien iaculis congue');
+insert into posts (contenidos_id, text) values (53317, 'aliquam lacus');
+
+--labels
+insert into labels (exclusividades_id, label) values (133, '733_vlb');
+insert into labels (exclusividades_id, label) values (258, '408_eca');
+insert into labels (exclusividades_id, label) values (383, '028_grt');
+insert into labels (exclusividades_id, label) values (508, '033_roj');
+insert into labels (exclusividades_id, label) values (633, '321_vwo');
+insert into labels (exclusividades_id, label) values (758, '333_tyl');
+insert into labels (exclusividades_id, label) values (883, '519_rlq');
+insert into labels (exclusividades_id, label) values (1008, '979_xgq');
+insert into labels (exclusividades_id, label) values (1133, '178_qqy');
+insert into labels (exclusividades_id, label) values (1258, '660_uqk');
+insert into labels (exclusividades_id, label) values (1383, '252_xnf');
+insert into labels (exclusividades_id, label) values (1508, '314_yfn');
+insert into labels (exclusividades_id, label) values (1633, '008_cwz');
+insert into labels (exclusividades_id, label) values (1758, '425_ihf');
+insert into labels (exclusividades_id, label) values (1883, '280_qdn');
+insert into labels (exclusividades_id, label) values (2008, '829_jev');
+insert into labels (exclusividades_id, label) values (2133, '813_ctu');
+insert into labels (exclusividades_id, label) values (2258, '749_prz');
+insert into labels (exclusividades_id, label) values (2383, '447_utp');
+insert into labels (exclusividades_id, label) values (2508, '817_dfx');
+
+--likes
+insert into likes (usuarios_id, contenidos_id) values (1, 3477);
+insert into likes (usuarios_id, contenidos_id) values (2, 7037);
+insert into likes (usuarios_id, contenidos_id) values (3, 10597);
+insert into likes (usuarios_id, contenidos_id) values (4, 14157);
+insert into likes (usuarios_id, contenidos_id) values (5, 17717);
+insert into likes (usuarios_id, contenidos_id) values (6, 21277);
+insert into likes (usuarios_id, contenidos_id) values (7, 24837);
+insert into likes (usuarios_id, contenidos_id) values (8, 28397);
+insert into likes (usuarios_id, contenidos_id) values (9, 31957);
+insert into likes (usuarios_id, contenidos_id) values (10, 35517);
+insert into likes (usuarios_id, contenidos_id) values (11, 39077);
+insert into likes (usuarios_id, contenidos_id) values (12, 42637);
+insert into likes (usuarios_id, contenidos_id) values (13, 46197);
+insert into likes (usuarios_id, contenidos_id) values (14, 49757);
+insert into likes (usuarios_id, contenidos_id) values (15, 53317);
